3ac860a026b50457c6f46251faef6f108fc15401
Carter David Henning <carterdavidhenning@gmail.com>
2021-05-03 17:29:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EZ Cloze
+A command line tool for generating cloze deletions for Anki from a list of sentences.
---
+++ rust/target/debug/deps/ezcloze-76acfeedaac5f33c.d
@@ -0,0 +1,5 @@
+c:\projects\ezcloze\target\debug\deps\ezcloze-76acfeedaac5f33c.rmeta: src\main.rs
+
+c:\projects\ezcloze\target\debug\deps\ezcloze-76acfeedaac5f33c.d: src\main.rs
+
+src\main.rs:
